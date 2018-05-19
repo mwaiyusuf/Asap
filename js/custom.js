1242a630a4731b4ddb7e451941ac24df9a40bc55
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-19 22:33:04 +0300

    e

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,6 @@
   var fullNames = [];
   for (var i = 0; i < firstNames.length; i++) {
     for (var j = 0; j < lastNames.length; j++) {
-//      fullNames.push(firstNames[i] + lastNames[j]);
+      fullNames.push(firstNames[i] + lastNames[j]);
 //    }
 // }
