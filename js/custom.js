211825fb151b4db99f71ab2f18e232aadfd0a4b9
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-19 22:30:35 +0300

    o

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
  var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
   var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
   var fullNames = [];
-//  for (var i = 0; i < firstNames.length; i++) {
+  for (var i = 0; i < firstNames.length; i++) {
 //    for (var j = 0; j < lastNames.length; j++) {
 //      fullNames.push(firstNames[i] + lastNames[j]);
 //    }
