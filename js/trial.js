0079c10c3ebd6dab15cb5addea80cefaafef7608
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-01 23:41:00 +0300

    t

---
+++ js/trial.js
@@ -0,0 +1,10 @@
+function cop(){
+//    var firstNames=["mohammed","yusuf","ali","zeid","faruq","salma"];
+//    var lastNames=["mwai","kamau","zubeir","abdi","alio","ayub"];
+//    var fullNames=[];
+//    for(i=0;i<firstNames.length;i++){
+//        for(j=0;j<lastNames.length;j++){
+//            fullNames.push(firstNames[i] + lastNames[j]);
+//        }
+//    }
+//}
