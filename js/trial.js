74f80f26ba1cba236c0b0eca46971ad4299a6e96
mohammed yusuf <mwaigalo5@gmail.com>
2018-06-02 08:35:50 +0300

    f

--- js/trial.js
+++ js/trial.js
@@ -1,5 +1,5 @@
 function cop(){
-//    var firstNames=["mohammed","yusuf","ali","zeid","faruq","salma"];
+    var firstNames=["mohammed","yusuf","ali","zeid","faruq","salma"];
 //    var lastNames=["mwai","kamau","zubeir","abdi","alio","ayub"];
 //    var fullNames=[];
 //    for(i=0;i<firstNames.length;i++){
